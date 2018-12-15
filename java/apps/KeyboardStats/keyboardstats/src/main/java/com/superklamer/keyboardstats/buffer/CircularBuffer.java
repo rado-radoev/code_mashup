799f6c04792d11de347341e245849039ebc6a58b
Radoslav Radoev <rradoev@gmail.com>
2018-12-15 00:54:31 -0800

    Started the key Stat app

---
+++ java/apps/KeyboardStats/keyboardstats/src/main/java/com/superklamer/keyboardstats/buffer/CircularBuffer.java
@@ -0,0 +1,71 @@
+package com.superklamer.keyboardstats.buffer;
+
+import com.superklamer.keyboardstats.exceptions.*;
+
+public class CircularBuffer<Integer> {
+
+
+	private int capacity;
+	private int head;
+	private int tail;
+	private int size;
+	private int data[];
+	
+	public CircularBuffer(int capacity) throws BufferIOException {
+		this.capacity = capacity;
+		head = 0;
+		tail = 0;
+		size = 0;
+		data = new int[capacity];
+	}
+	
+	public void write(int value) throws BufferIOException {
+		
+		if (isBufferFull()) {
+			throw new BufferIOException("Tried to write to full buffer");
+		}
+		
+		data[tail] = value;
+		tail = (tail + 1) % capacity;
+		size++;
+	}
+	
+	public int read() throws BufferIOException {
+		
+		if (isBufferEmpty())
+			throw new BufferIOException("Tried to read from empty buffer");
+		
+
+		int value = data[head];
+		head = (head + 1) % capacity;
+		size--;
+		
+		return value;
+	}
+	
+	public void overwrite(int value) throws BufferIOException {
+		if (size < capacity) write(value);
+		else {
+			data[head] = value;
+			head = (head + 1) % capacity;	
+		}
+		
+		
+		
+	}
+	
+	public void clear() {
+		head = 0; tail = 0; size = 0;
+		
+	}
+	
+	private boolean isBufferFull() {
+		return size == capacity;
+		
+		
+	}
+	
+	private boolean isBufferEmpty() {
+		return size == 0;
+	}
+}
---
+++ java/apps/KeyboardStats/keyboardstats/src/main/java/com/superklamer/keyboardstats/exceptions/BufferIOException.java
@@ -0,0 +1,8 @@
+package com.superklamer.keyboardstats.exceptions;
+
+public class BufferIOException extends Exception {
+
+    public BufferIOException(String message) {
+        super(message);
+    }
+}
