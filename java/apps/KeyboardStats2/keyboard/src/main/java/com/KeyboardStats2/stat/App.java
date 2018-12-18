4a829dd7fd977863186dcfbdbe3cc9a734fa2ee1
rradoev@gmail.com <rradoev@gmail.com>
2018-12-18 08:51:36 -0800

    getting new proj

---
+++ java/apps/KeyboardStats2/keyboard/src/main/java/com/KeyboardStats2/stat/App.java
@@ -0,0 +1,13 @@
+package com.KeyboardStats2.stat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
