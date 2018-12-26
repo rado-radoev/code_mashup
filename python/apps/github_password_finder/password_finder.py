b1217df2ccb2fcadad850465d459ef6e1839d882
Radoslav Radoev <rradoev@gmail.com>
2018-12-25 22:56:39 -0800

    fuck it

--- python/apps/github_password_finder/password_finder.py
+++ python/apps/github_password_finder/password_finder.py
@@ -4,5 +4,19 @@
 Created on Sat Dec 22 15:44:22 2018
 
 @author: superlamer
+
+search and find repositories
 """
+
+import requests, json
+
+url = 'https://api.github.com/search/repositories?q=java&sort=stars&order=asc'
+
+response = requests.get(url)
+response_json = response.json()
+
+
+
+
+
 
