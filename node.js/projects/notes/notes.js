8d99f89af784c84c013ef27e66b3e97e9f6831e9
Radoslav Radoev <rradoev@gmail.com>
2019-01-09 23:43:41 -0800

    node js notes app

--- node.js/projects/notes/notes.js
+++ node.js/projects/notes/notes.js
@@ -1,1 +1,42 @@
 console.log('Starting notes.js');
+
+const fs = require('fs');
+
+var addNote = (title, body) => {
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch (e) { }
+
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
+};
+
+var getAll = () => {
+  console.log('Returning list of notes');
+};
+
+var removeNote = (title) => {
+  console.log('Removing note', title);
+};
+
+var getNote = (title) => {
+  console.log('Reading note', title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+}
