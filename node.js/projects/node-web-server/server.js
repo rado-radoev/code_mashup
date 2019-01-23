5a9fb03d2fbfee3da0e3c2580bc88aad185e45d5
Radoslav Radoev <rradoev@gmail.com>
2019-01-23 00:03:52 -0800

    commit

--- node.js/projects/node-web-server/server.js
+++ node.js/projects/node-web-server/server.js
@@ -1,22 +1,40 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: Andrew,
-    likes: [
-      'Biking',
-      'Cities'
-    ]
+  // res.send('<h1>Hello Express!</h1>');
+  // res.send({
+  //   name: Andrew,
+  //   likes: [
+  //     'Biking',
+  //     'Cities'
+  //   ]
+  // });
+  res.render('home.hbs', {
+    pageTitle: 'This is home',
+    welcomeMessage: 'Well "Hello There!"'
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  // res.send('About page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
 });
 
 app.get('/bad', (req, res) => {
