7768927612da3bdc75c4213158e9403830d1376d
Radoslav Radoev <rradoev@gmail.com>
2019-01-24 00:11:46 -0800

    update

--- node.js/projects/node-web-server/server.js
+++ node.js/projects/node-web-server/server.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unabel to write to server.log');
+    }
+  });
+
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs', {
+    maintenanceMessage: 'Website is under maintenance'
+  });
+});
+
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('printMessage', (message) => {
+  return message;
+});
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
