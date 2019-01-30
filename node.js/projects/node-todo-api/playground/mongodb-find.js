5d89da9560a3d72c6b92041145fde45bacf6eb21
Radoslav Radoev <rradoev@gmail.com>
2019-01-29 22:28:46 -0800

    test

--- node.js/projects/node-todo-api/playground/mongodb-find.js
+++ node.js/projects/node-todo-api/playground/mongodb-find.js
@@ -41,7 +41,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log(JSON.stringify(docs, undefined, 2));
   }, (err) => {
     console.log('Unable to fetch todos', err);
-  });
+  });   
 
 
   client.close();
