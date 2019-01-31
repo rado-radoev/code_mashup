6510d9f6f82be848d7c1397e97850f7911159ee5
Radoslav Radoev <rradoev@gmail.com>
2019-01-31 00:37:26 -0800

    mongoose create model and documents in mongodb

--- node.js/projects/node-todo-api/server/server.js
+++ node.js/projects/node-todo-api/server/server.js
@@ -1,3 +1,40 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/TodoApp', {err, db} =? {});
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todos', {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+});
+
+
+// var newTodo = new Todo({
+//   text: 'Cook dinner'
+// });
+//
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save Todo')
+// });
+
+
+var secondTodo = new Todo({
+  text: 'Cook breakfast',
+  completed: false,
+  completedAt: 0
+});
+
+secondTodo.save().then((doc) => {
+  console.log('Successfully saved todo', JSON.stringify(doc, undefined, 2));
+}, (e) => {
+  console.log('Unable to save todo');
+});
