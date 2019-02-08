37612148cc5330b3d57d639f914a6c57aed244de
Radoslav Radoev <rradoev@gmail.com>
2019-02-07 23:42:02 -0800

    freaking tokens man

--- node.js/projects/node-todo-api/server/server.js
+++ node.js/projects/node-todo-api/server/server.js
@@ -99,6 +99,17 @@ app.patch('/todos/:id', (req, res) => {
     }).catch((e) => res.status(400).send());
 });
 
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  var user = new User(body);
+  user.save().then((user) => {
+    res.send(user);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
