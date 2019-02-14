f35996a89f6196df31484386b2472982397e2d71
Radoslav Radoev <rradoev@gmail.com>
2019-02-13 23:07:13 -0800

    Added tests for DELETE /users/me/token

--- node.js/projects/node-todo-api/server/tests/server.test.js
+++ node.js/projects/node-todo-api/server/tests/server.test.js
@@ -315,7 +315,46 @@ describe('POST /users/login', () => {
 });
 
 describe('DELETE /users/me/token', () => {
-  it('should remove auth toke on logout', (done) => {
+  it('should remove auth token on logout', (done) => {
+    let token = users[0].tokens[0].token;
 
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findOne(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return error if token does not exist', (done) => {
+    var token = users[0].tokens[0].token.concat('1');
+
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', token)
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findByToken(users[0]._id).then((user) => {
+          expect(users[0].tokens[0].token).toNotEqual(token);
+        }).catch((e) => done(e));
+      });
   });
 });
