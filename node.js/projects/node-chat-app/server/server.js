8d138808371e0dc8f486a951dc32c6d65065217d
Radoslav Radoev <rradoev@gmail.com>
2019-02-15 00:10:58 -0800

    added HBS. Will use it later

--- node.js/projects/node-chat-app/server/server.js
+++ node.js/projects/node-chat-app/server/server.js
@@ -1,14 +1,15 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const port = process.env.PORT || 3000;
-
 var publicPath = path.join(__dirname, '/../public');
 
 var app = express();
+hbs.registerPartials(path.join(__dirname, './../views/partials'));
+app.set('view engine', 'hbs');
 
 app.use(express.static(publicPath));
-
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
