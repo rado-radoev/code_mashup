b2d05bb6983a105d7ff4aa6314062d3d648fc8e4
Radoslav Radoev <rradoev@gmail.com>
2019-02-16 00:03:22 -0800

    Playing with socket.io

--- node.js/projects/node-chat-app/server/server.js
+++ node.js/projects/node-chat-app/server/server.js
@@ -1,16 +1,35 @@
 const path = require('path');
-const express = require('express');
 const hbs = require('hbs');
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
 var publicPath = path.join(__dirname, '/../public');
-
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 hbs.registerPartials(path.join(__dirname, './../views/partials'));
 app.set('view engine', 'hbs');
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New User connected');
+  socket.on('disconnect', (reason) => {
+    if (reason === 'server namespace disconnect') {
+      console.log('Server closed connection');
+    }
+    else if (reason === 'client namespace disconnect') {
+      console.log('Client closed connection');
+    }
+    else {
+      console.log('Connection just closed');
+    }
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
