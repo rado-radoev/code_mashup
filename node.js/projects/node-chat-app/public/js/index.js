840948e38560101e1922b598dc1858c5ea0e9bff
Radoslav Radoev <rradoev@gmail.com>
2019-02-18 22:22:05 -0800

    emit new message on create message

--- node.js/projects/node-chat-app/public/js/index.js
+++ node.js/projects/node-chat-app/public/js/index.js
@@ -2,17 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  // socket.emit('createEmail', {
-  //   to: 'jen@example.com',
-  //   text: 'Hey this is Rado'
-  // });
-
-  socket.emit('createMessage', {
-    from: 'lame@example.com',
-    text: 'i am stupid'
-  });
-
 });
 
 socket.on('disconnect', function () {
@@ -22,10 +11,6 @@ socket.on('disconnect', function () {
 socket.on('reconnect', function (attemptNumber) {
   console.log('Reconnected');
 });
-
-// socket.on('newEmail', function (email) {
-//   console.log('New email', email);
-// });
 
 socket.on('newMessage', function (message) {
   console.log('New message', message);
--- node.js/projects/node-chat-app/server/server.js
+++ node.js/projects/node-chat-app/server/server.js
@@ -18,24 +18,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New User connected');
 
-  socket.emit('newMessage', {
-    from: 'johnwick@example.com',
-    text: 'baba yaga',
-    createdAt: Date.now()
-  });
-
-  // socket.emit('newEmail', {
-  //   from: 'mike@example.com',
-  //   text: 'Hey. What is going on?',
-  //   createdAt: 123
-  // });
-  //
-  // socket.on('createEmail', (newEmail) => {
-  //   console.log('createEmail', newEmail);
-  // });
-
   socket.on('createMessage', (message) => {
-    console.log('createMessage', message);
+    //console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
 
