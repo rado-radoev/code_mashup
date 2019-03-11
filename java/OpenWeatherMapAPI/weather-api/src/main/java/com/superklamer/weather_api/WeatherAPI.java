f417044a786f199192e8532584bbca3bdac0bed3
Radoslav Radoev <rradoev@gmail.com>
2019-03-10 23:34:20 -0700

    palying with weather api

--- java/OpenWeatherMapAPI/weather-api/src/main/java/com/superklamer/weather_api/WeatherAPI.java
+++ java/OpenWeatherMapAPI/weather-api/src/main/java/com/superklamer/weather_api/WeatherAPI.java
@@ -3,16 +3,26 @@ package com.superklamer.weather_api;
 import com.sun.jersey.api.client.ClientResponse;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.List;
 import java.util.Map;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.google.gson.JsonArray;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 
 import javassist.expr.Instanceof;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 public class WeatherAPI {
 
@@ -41,6 +51,16 @@ public class WeatherAPI {
 		JsonNode jsonNode = objectMapper.readTree(output); 
 		System.out.println(jsonNode.get("weather").get(0).get("main"));
 		
+		JsonReader reader = Json.createReader(new StringReader(output));
+		JsonObject obj = reader.readObject();
+		
+		reader.close();
+		
+		System.out.println(obj.getJsonArray("weather").get(0));
+		
+		ArrayNode arr = (ArrayNode) jsonNode.get("weather"); 
+		System.out.println(arr.get(0).get("id"));
+		
 //		Weather weather = objectMapper.readValue(output, Weather.class);
 //		
 //		System.out.println(weather);
@@ -53,3 +73,4 @@ public class WeatherAPI {
 	}
 
 }
+;
