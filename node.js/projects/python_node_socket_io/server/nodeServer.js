900b0a7d550ba391d1c18cdd8ae47c428aabfa36
Radoslav Radoev <rradoev@gmail.com>
2019-03-19 00:41:31 -0700

    adding function to get current location from browser

--- node.js/projects/python_node_socket_io/server/nodeServer.js
+++ node.js/projects/python_node_socket_io/server/nodeServer.js
@@ -74,3 +74,25 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
   console.log('Server is up and running on port ', port)
 })
+
+var coords = () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser')
+  
+    var options = {
+      enableHighAccuracy: false,
+      timeout: 5000,
+      maximumAge: 6000
+    }
+
+    function success(pos) {
+      return pos.coords
+    }
+  
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`)
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options)
+  }
+}
