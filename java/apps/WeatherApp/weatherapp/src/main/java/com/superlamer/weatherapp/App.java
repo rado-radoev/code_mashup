ac607ab7ef280547b6b7a32f5e4473d5e9307af1
Radoslav Radoev <rradoev@gmail.com>
2019-03-28 00:53:28 -0700

    trying to get jax-rs working

--- java/apps/WeatherApp/weatherapp/src/main/java/com/superlamer/weatherapp/App.java
+++ java/apps/WeatherApp/weatherapp/src/main/java/com/superlamer/weatherapp/App.java
@@ -10,6 +10,7 @@ import com.superlamer.weatherapp.City.JSONParser;
 import com.superlamer.weatherapp.City.ListDownloader;
 import com.superlamer.weatherapp.DB.Database;
 import com.superlamer.weatherapp.Logger.Log;
+import com.superlamer.weatherapp.weather.Weather;
 
 
 /**
@@ -21,17 +22,19 @@ public class App
     public static void main( String[] args ) throws IOException
     {
     	
-    	ListDownloader download = new ListDownloader();
-    	String url = "https://raw.githubusercontent.com/superklamer/code_mashup/master/java/apps/city.list.json";
-    	//download.downloadFile(new URL(url));
-        
-    	JSONParser jp = new JSONParser();
-    	City sd = jp.findCity("Murava", new File("/tmp/city.list.json"));
-    	
-    	Database monDb = new Database();
-    	Document doc = monDb.toDocument(sd);
-    	boolean added = monDb.addNewDBEntry(doc);
-    	Log.log().info(added);
+//    	ListDownloader download = new ListDownloader();
+//    	String url = "https://raw.githubusercontent.com/superklamer/code_mashup/master/java/apps/city.list.json";
+//    	//download.downloadFile(new URL(url));
+//        
+//    	JSONParser jp = new JSONParser();
+//    	City sd = jp.findCity("Murava", new File("/tmp/city.list.json"));
+//    	
+//    	Database monDb = new Database();
+//    	Document doc = monDb.toDocument(sd);
+//    	boolean added = monDb.addNewDBEntry(doc);
+//    	Log.log().info(added);
+
+    	Weather w = new Weather();
     	
     }
 }
--- java/apps/WeatherApp/weatherapp/src/main/java/com/superlamer/weatherapp/weather/Weather.java
+++ java/apps/WeatherApp/weatherapp/src/main/java/com/superlamer/weatherapp/weather/Weather.java
@@ -1,26 +1,37 @@
 package com.superlamer.weatherapp.weather;
 
-import com.sun.
-
 public class Weather {
 
 	public Weather() {
-		
+		getWeather();
 	}
 	
-	public String getWeather(int id) {
-		
-	}
+//	public String getWeather(int id) {
+//		
+//	}
+//	
+//	public String getWeather(String cityName) {
+//		
+//	}
+//	
+//	public String getWeather(Coord coord) {
+//		
+//	}
 	
-	public String getWeather(String cityName) {
+	private void getWeather() {
+//https://api.openweathermap.org/data/2.5/weather?q=London&appid=6a0326b54ac62aae38ee842128683084"		
+		String webURI = "https://api.openweathermap.org/data/2.5/weather";
 		
-	}
-	
-	public String getWeather(Coord coord) {
-		
-	}
-	
-	private String getWeather() {
-		Client
+		Client client = ClientBuilder.newClient();
+		client.target(webURI)
+				 .queryParam("id", 5391811)
+				 .queryParam("appid", "6a0326b54ac62aae38ee842128683084")
+				 .queryParam("mode", "json")
+				 .queryParam("units", "metric")
+				 .request(MediaType.APPLICATION_JSON)
+				 .buildGet();
+
+		System.out.println(client);
+
 	}
 }
