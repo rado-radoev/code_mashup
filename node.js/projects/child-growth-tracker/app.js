c9b09a485fe6d6b4b6e5b61131329cc4c18cd605
Radoslav Radoev <rradoev@gmail.com>
2019-06-25 00:14:38 -0700

    setting up jwt

--- node.js/projects/child-growth-tracker/app.js
+++ node.js/projects/child-growth-tracker/app.js
@@ -7,6 +7,7 @@ var hbs = require('hbs')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testsRouter = require('./routes/tests');
 var partialsDir = path.join(__dirname, './views/partials')
 
 var app = express();
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/', usersRouter);
+app.use('/', testsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ node.js/projects/child-growth-tracker/routes/tests.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', async function(req, res, next) {
+  next();
+});
+
+router.get('/test', async function(req, res, next) {
+  const jwt = require('jsonwebtoken')
+
+  const myFunc = async () => {
+    const token = jwt.sign({ _id: 'abc123'}, 'thisismynewcourse', {expiresIn: '7 days'})
+    console.log(token)
+
+    const data = jwt.verify(token, 'thisismynewcourse')
+    console.log(data)
+  }
+
+  myFunc();
+});
+
+
+module.exports = router;
