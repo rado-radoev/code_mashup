7678d859fd850e3304f8ec1e50e2b186550ec663
Radoslav Radoev <rradoev@gmail.com>
2019-07-05 00:17:23 -0700

    setting up derby. Gosh I forgot so much

--- java/apps/TaskManager/taskmanager/src/main/java/com/superlamer/taskmanager/App.java
+++ java/apps/TaskManager/taskmanager/src/main/java/com/superlamer/taskmanager/App.java
@@ -1,5 +1,10 @@
 package com.superlamer.taskmanager;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String url = "jdbc:mysql://localhost:3306/taskdb?autoReconnect=true&useSSL=false";
+    	String user = "root";
+    	String password = "rado";
+        try (Connection myConn = DriverManager.getConnection(url, user, password);) {
+        	Statement myStatement = myConn.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
