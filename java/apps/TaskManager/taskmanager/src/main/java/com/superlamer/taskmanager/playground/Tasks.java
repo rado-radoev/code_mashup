459a7798996f2a8ebcc1ae68a6781631055de4f1
Radoslav Radoev <rradoev@gmail.com>
2019-07-17 23:53:12 -0700

    still deciding on the layout manager

---
+++ java/apps/TaskManager/taskmanager/src/main/java/com/superlamer/taskmanager/playground/Tasks.java
@@ -0,0 +1,53 @@
+package com.superlamer.taskmanager.playground;
+
+import java.util.Date;
+
+public class Tasks {
+	
+	private String task;
+	private int duration;
+	private Date date;
+	private Boolean completed;
+	
+	public Tasks(String task, int duration, Date date, Boolean completed) {
+		this.task = task;
+		this.duration = duration;
+		this.date = date;
+		this.completed = completed;
+	}
+
+	public String getTask() {
+		return task;
+	}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setTime(int duration) {
+		this.duration = duration;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Boolean getCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(Boolean completed) {
+		this.completed = completed;
+	}
+	
+	
+
+}
--- java/apps/TaskManager/taskmanager/src/main/java/com/superlamer/taskmanager/playground/View.java
+++ java/apps/TaskManager/taskmanager/src/main/java/com/superlamer/taskmanager/playground/View.java
@@ -4,13 +4,23 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import javax.swing.Box;
 import javax.swing.GroupLayout;
+import javax.swing.JCheckBox;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
 public class View extends JFrame {
@@ -20,31 +30,74 @@ public class View extends JFrame {
 		setResizable(true);
 	}
 	
+	private void addTasksToPane(final Container pane) {
+		JPanel taskListPane = new JPanel();
+		
+		GridLayout gr = new GridLayout();
+		taskListPane.setLayout(gr);
+		
+		Tasks task = new Tasks("Test Task", 86400, new Date(), false);
+		
+		JCheckBox checkBox = new JCheckBox();
+		JTextField taskField = new JTextField(task.getTask());
+		JTextField durationField = new JTextField(task.getDuration());
+		JTextField dateField = new JTextField(task.getDate().toString());
+		JTextField completedField = new JTextField(task.getCompleted().toString());
+		
+		if (task.getCompleted()) {
+			checkBox.setSelected(true);
+		}
+		
+		List<JComponent> taskComponents = new ArrayList<JComponent>();
+		taskComponents.add(checkBox);
+		taskComponents.add(taskField);
+		taskComponents.add(durationField);
+		taskComponents.add(dateField);
+		taskComponents.add(completedField);
+		
+		for (JComponent comp : taskComponents) {
+			taskListPane.add(comp);
+		}
+		
+		pane.add(taskListPane, BorderLayout.NORTH);
+	}
+	
 	private void addComponentsToPane(final Container pane) {
 	
 		JPanel headers = new JPanel();
-//		headers.setLayout(new GridLayout(1, 4, 20, 0));
-		
 		
 		GroupLayout groupLayout = new GroupLayout(headers);
 		groupLayout.setAutoCreateContainerGaps(true);
 		groupLayout.setAutoCreateGaps(true);
 		
-		GroupLayout.SequentialGroup hGroup = groupLayout.createSequentialGroup();
-		hGroup
-			.addComponent(new JLabel("Task"))
-			.addComponent(new JLabel("Time"))
-			.addComponent(new JLabel("Date"))
-			.addComponent(new JLabel("Completed"));
+		JLabel taskLabel = new JLabel("Task");
+		JLabel timeLabel = new JLabel("Time");
+		JLabel dateLabel = new JLabel("Date");
+		JLabel completedLabel = new JLabel("Completed");
 		
-		groupLayout.setHorizontalGroup(hGroup);
+		taskLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		timeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		dateLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		completedLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		headers.setLayout(new GridLayout(1, 4, 20, 0));
+		headers.add(taskLabel);
+		headers.add(timeLabel);
+		headers.add(dateLabel);
+		headers.add(completedLabel);
+		
 		
 		pane.add(headers, BorderLayout.NORTH);
+		pane.add(new JSeparator(SwingConstants.HORIZONTAL));
 	}
 	
 
 	private static void createAndShowGUI() {
 		View frame = new View("Tasks app");
+		
+		GridLayout fl = new GridLayout(2, 1);
+		frame.setLayout(fl);
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		frame.setVisible(true);
@@ -52,6 +105,7 @@ public class View extends JFrame {
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		frame.setSize(400, screenSize.height);
 		frame.addComponentsToPane(frame.getContentPane());
+		frame.addTasksToPane(frame.getContentPane());
 	}
 	
 	public static void main(String[] args) {
