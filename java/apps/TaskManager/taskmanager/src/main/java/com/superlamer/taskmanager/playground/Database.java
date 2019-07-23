621c99c5851d5ee820ecf08bca84ec74b2d55c5b
Radoslav Radoev <rradoev@gmail.com>
2019-07-23 00:09:41 -0700

    have the db stuff. need to test them and then it is displaying them in the form

--- java/apps/TaskManager/taskmanager/src/main/java/com/superlamer/taskmanager/playground/Database.java
+++ java/apps/TaskManager/taskmanager/src/main/java/com/superlamer/taskmanager/playground/Database.java
@@ -2,22 +2,24 @@ package com.superlamer.taskmanager.playground;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class Database {
 	
 	private static Database instance;
 	private Connection myConn;
+	private String url = "jdbc:mysql://127.0.0.1:3306/taskdb?serverTimezone=UTC";
+	private String user = "root";
+	private String password = "rado";
 	
-	public Database () {
-		String url = "jdbc:mysql://127.0.0.1:3306/taskdb?serverTimezone=UTC";
-    	String user = "root";
-    	String password = "rado";
-    	
+	public Database () { 	
     	try {
 			myConn = DriverManager.getConnection(url, user, password);
 		} catch (SQLException e) {
@@ -37,60 +39,176 @@ public class Database {
 		return myConn;
 	}
 	
-	public void getSingleRecordFromDB() { }
-	
-	public void addRecordToDB() { }
-	
-	public void addMultipleRecordsToDB() { }
-	
-	public void removeRecordFromDB() { }
-	
-	public void removeMultiplRecordsFromDB() { }
-	
-	public void disconnectFromDB() { }
-	
-	public void queryRecordsByID() { }
-	
-	public void queryRecordsByTaskName() { }
-	
-	public void queryRecordsByTime() { }
-	
-	public void queryRecordsByDate() { }
-	
-	public void queryRecordsByCompletionStatus() { }
-	
-	public void displayRecordsToConsole() {
-		
-	} 
-	
-	public void getAllDataFromDB() {
-		String url = "jdbc:mysql://127.0.0.1:3306/taskdb?serverTimezone=UTC";
-    	String user = "root";
-    	String password = "rado";
-        try (Connection myConn = DriverManager.getConnection(url, user, password);) {
-        	Statement myStatement = myConn.createStatement();
-        	String sql = "SELECT * FROM tasks";
-        	
-        	ResultSet rs = myStatement.executeQuery(sql);
-        	ResultSetMetaData rsmd = rs.getMetaData();
-        	int columnCount = rsmd.getColumnCount();
-        	ArrayList<String> columnNames = new ArrayList<String>();
-        	
-        	// Column count starts from 1
-        	for (int i = 1; i <= columnCount; i++) {
-        		String name = rsmd.getColumnName(i);
-        		columnNames.add(name);
-        	}
-        	
-        	while (rs.next()) {
-        		for (String column : columnNames) {
-					System.out.println(column + ": " + rs.getString(column));
-				}
-        	}
-        	
+	private static List<Tasks> convertResultSetToList(ResultSet rs) {
+		List<Tasks> tasks = new ArrayList<Tasks>();
+
+		try {
+			while (rs.next()) {
+				tasks.add(new Tasks(rs.getString("tasks"),
+									rs.getInt("time"),
+									rs.getDate("date"),
+									rs.getBoolean("completed")
+						));
+			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return tasks;
+	}
+	
+	
+	public void addRecordToDB(Tasks task) {
+		Statement statement = null;
+		
+		try {
+			statement = myConn.createStatement();
+			String sqlUpdate = String.format("INSERT INTO task " +  
+			"VALUES (%s, %s, %t, %b)", 
+			task.getTask(),
+			task.getDuration(),
+			task.getDate(),
+			task.getCompleted());
+			
+			statement.executeUpdate(sqlUpdate);
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public void removeRecordFromDB(Tasks task) {
+		Statement statement = null;
+		
+		try {
+			statement = myConn.createStatement();
+			String sqlUpdate = String.format("DELETE FROM task where task = VALUES (%s, %s, %t, %b)",
+					task.getTask(),
+					task.getDuration(),
+					task.getDate(),
+					task.getCompleted());
+			
+			statement.executeUpdate(sqlUpdate);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+		
+	public void disconnectFromDB() {
+		if (myConn != null) {
+			try {
+				myConn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public List<Tasks> queryRecordsByID(String id) { 
+		List<Tasks> tasks = convertResultSetToList(runQuery("SELECT * FROM task WHERE id = " + id));
+		
+		return tasks;
+	} 
+	
+	public List<Tasks> queryRecordsByTaskName(String taskName) {
+		List<Tasks> tasks = convertResultSetToList(runQuery("SELECT * FROM task WHERE tasks = " + taskName));
+		
+		return tasks;
+	}
+	
+	public List<Tasks> queryRecordsByTime(int time, int minTime, int maxTime) { 
+		List<Tasks> tasks = convertResultSetToList(runQuery("SELECT * FROM task WHERE time <= " + maxTime + " AND time => " + minTime));
+		
+		return tasks;
+	}
+	
+	public List<Tasks> queryRecordsByDate(Date date) { 
+		List<Tasks> tasks = convertResultSetToList(runQuery("SELECT * FROM task WHERE (cast [date] as date) = " + date));
+		
+		return tasks;
+	}
+	
+	public List<Tasks> queryRecordsByCompletionStatus(boolean completionStatus) {
+		List<Tasks> tasks = convertResultSetToList(runQuery("SELECT * FROM task WHERE completed = " + completionStatus));
+		
+		return tasks;
+	}
+	
+	public void displayRecordsToConsole(ResultSet rs) { 
+		try {
+			ResultSetMetaData rsmd = rs.getMetaData();
+	    	int columnCount = rsmd.getColumnCount();
+	    	ArrayList<String> columnNames = new ArrayList<String>();
+	    	
+	    	// Column count starts from 1
+	    	for (int i = 1; i <= columnCount; i++) {
+	    		String name = rsmd.getColumnName(i);
+	    		columnNames.add(name);
+	    	}
+	    	
+	    	while (rs.next()) {
+	    		for (String column : columnNames) {
+					System.out.println(column + ": " + rs.getString(column));
+				}
+	    	}
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		}
+	}
+	
+	public ResultSet runQuery(String query) {
+		
+		Statement statement = null;
+		ResultSet rs = null;
+		try {
+			statement = myConn.createStatement();
+			rs = statement.executeQuery(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		 
+		return rs;
+	}
+	
+	public void getAllDataFromDB() {
+        
+
+		ResultSet rs = runQuery("SELECT * FROM tasks");
+		displayRecordsToConsole(rs);
+ 
+	}
+	
+	
+	
+//	public void getAllDataFromDB() {
+//		String url = "jdbc:mysql://127.0.0.1:3306/taskdb?serverTimezone=UTC";
+//    	String user = "root";
+//    	String password = "rado";
+//        try (Connection myConn = DriverManager.getConnection(url, user, password);) {
+//        	Statement myStatement = myConn.createStatement();
+//        	String sql = "SELECT * FROM tasks";
+//        	
+//        	ResultSet rs = myStatement.executeQuery(sql);
+//        	ResultSetMetaData rsmd = rs.getMetaData();
+//        	int columnCount = rsmd.getColumnCount();
+//        	ArrayList<String> columnNames = new ArrayList<String>();
+//        	
+//        	// Column count starts from 1
+//        	for (int i = 1; i <= columnCount; i++) {
+//        		String name = rsmd.getColumnName(i);
+//        		columnNames.add(name);
+//        	}
+//        	
+//        	while (rs.next()) {
+//        		for (String column : columnNames) {
+//					System.out.println(column + ": " + rs.getString(column));
+//				}
+//        	}
+//        	
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+
+
 }
