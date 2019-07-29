70596b94ba9049014258a37d4695add23f5f3746
Radoslav Radoev <rradoev@gmail.com>
2019-07-29 00:17:16 -0700

    multithreaded the page scraping too. Fixed a few bugs with products missing information. Refactored

--- python/scripts/freenalcing/web_scrape/scraper.py
+++ python/scripts/freenalcing/web_scrape/scraper.py
@@ -5,6 +5,7 @@ import math
 import queue
 import concurrent.futures
 import time
+import threading
 import bs4 as bs
 from product import Product
 
@@ -39,25 +40,44 @@ def scraping(url):
 
     items = [item for item in soup.find_all(class_='product-item')]
 
-    for item in items: 
+    # slice list to two lists
+    splitted_items = split_list(items)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        # start = time.time()
+        futures = [executor.submit(get_product, ls) for ls in splitted_items]
+        # end = time.time()
+        # print("Time Taken: {:.6f}s".format(end-start))
+
+
+def split_list(list_to_split):
+    half_items = len(list_to_split)//2
+    return list_to_split[:half_items], list_to_split[half_items:]
+
+def get_product(items_list):
+    scraper = cfscrape.create_scraper()
+    for item in items_list: 
         try:
             item_img = item.findAll('img')[0]['data-original']
             item_name = item.find('a', class_='product-item-link').text
             item_link = item.find('a', class_='product-item-link').get('href')
 
-            item_sauce = requests.get(item_link)
-            item_soup = bs.BeautifulSoup(item_sauce.text, 'lxml')
+            item_sauce = scraper.get(item_link).content
+            item_soup = bs.BeautifulSoup(item_sauce, 'lxml')
 
             product = item_soup.find(class_='product-info-price')
             price = product.find('div', class_='price-final_price')
-            on_sale = item_soup.find('div', class_='amasty-label-text')
-            product_id = price.attrs.get('data-product-id')
-            old_price = price.find('div', {'id': "old-price-{}".format(product_id)})
+            if price:
+                on_sale = item_soup.find('div', class_='amasty-label-text')
+                product_id = price.attrs.get('data-product-id')
+                old_price = price.find('div', {'id': "old-price-{}".format(product_id)})
 
-            if on_sale and old_price:
-                old_price = old_price.find(class_='price').text
-            final_price = price.find('div', {'id': "product-price-{}".format(product_id)}).find(class_='price').text
-            # sku = item_soup.find('table', {'id': 'product-attribute-specs-table'}).tr.td.text
+                if on_sale and old_price:
+                    old_price = old_price.find(class_='price').text
+                final_price = price.find('div', {'id': "product-price-{}".format(product_id)}).find(class_='price').text
+                # sku = item_soup.find('table', {'id': 'product-attribute-specs-table'}).tr.td.text
+            else:
+                old_price = 0
+                final_price = 0
 
             for tr in item_soup.find('table'): 
                 td = tr.findAll('td')
@@ -65,7 +85,8 @@ def scraping(url):
                     if d.get('data-th') == 'SKU':
                         sku = d.text
                     elif d.get('data-th') == 'Product Reference Number':
-                        product_reference_number = d.text
+                        product_reference_number = d.text if d.text != None else 0
+            
 
             pr = Product(
                 item_name, 
@@ -78,8 +99,9 @@ def scraping(url):
             
             products_list.put(pr)
             print('qsize is: ' + str(products_list.qsize()))
-        except:
+        except Exception as e:
             print('Product skipped')
+            print(str(e))
 
 
 if __name__ == '__main__':
