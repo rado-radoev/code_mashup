9eb73381bce91fd6d2635f3f9e298f1355be60aa
Radoslav Radoev <Radoslav.Radoev@sharp.com>
2019-08-26 13:56:36 -0700

    added prdoucts model

--- python/Django/producthunt-project/products/models.py
+++ python/Django/producthunt-project/products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.URLField()
+    pub_date = models.DateField(auto_now=True)
+    votes_total = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='icons/')
+    body = models.TextField()
+    hunetr = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime("%b %e %Y")
