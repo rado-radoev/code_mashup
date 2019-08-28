26893bbfd1d6653011267fb8e4ba3fa58ba2ae44
Radoslav Radoev <Radoslav.Radoev@sharp.com>
2019-08-28 15:59:25 -0700

    playing with django sockets

---
+++ python/Django/chatenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\radra\github\code_mashup\python\django\chatenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
