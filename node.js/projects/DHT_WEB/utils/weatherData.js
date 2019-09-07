cc661cffcbde8679208d5eb25b8d0fb8c9ecfd90
Radoslav Radoev <rradoev@gmail.com>
2019-09-06 23:48:53 -0700

    dht_web is working now the python code

---
+++ node.js/projects/DHT_WEB/utils/weatherData.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const getLatestData = (url, callback) => {
+    var url = url;
+
+    request({url, json: true}, (error, response) => {
+        if (error) {
+            callback('Unable to connect to server', undefined);
+        } else if (response.body.length === 0) {
+            callback('Unable to fetch data from server', undefined);
+        } else {
+            callback(undefined, response.body[0])
+        }
+    });
+}
+
+module.exports = getLatestData;
