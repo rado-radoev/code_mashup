2288dff58bec64599748212a860b20005fc3a7de
Radoslav Radoev <Radoslav.Radoev@sharp.com>
2019-09-11 10:39:15 -0700

    Working on the html implemntation.

--- node.js/projects/raspbery-led-webserver/PIR/public/javascripts/client.js
+++ node.js/projects/raspbery-led-webserver/PIR/public/javascripts/client.js
@@ -1,5 +1,42 @@
 const socket = io();
 
+/* Events */
+document.getElementById('btn-on').addEventListener('click', function() {
+    btnControl('btn-on');
+});
+document.getElementById('btn-off').addEventListener('click', function() {
+    btnControl('btn-off');
+});
+
+/* Functions */
+function convertStatusToString(systemStatus) {
+    return (systemStatus ? "Armed": "Disarmed");
+}
+
+function btnControl(btn) {
+    socket.emit('btnPressed', btn);
+    
+    if (btn === 'btn-on') {
+        document.getElementById('btn-on').disabled = true;
+        document.getElementById('btn-off').disabled = false;
+    } else {
+        document.getElementById('btn-on').disabled = false;
+        document.getElementById('btn-off').disabled = true;
+    }
+}
+
+socket.on('newSystemStatus', (systemStatus) => {
+    var currStatus = document.getElementById('system-status').innerHTML;
+    var newStatus = currStatus.substring(0 , currStatus.indexOf(': ')) + `: ${convertStatusToString(systemStatus)}`;
+    document.getElementById('system-status').innerHTML = newStatus;
+    console.log(`System is currently ${convertStatusToString(systemStatus)}`);
+});
+
 socket.on('connected', (data) => {
-    console.log(`Message from server ${data}`);
+    console.log(`Message from server: ${data.payload}`);
+
+    socket.emit('getSystemStatus', (callbackData) => {
+        var systemStatus = callbackData ? "Armed" : "Disarmed"
+        console.log(`System is currently ${systemStatus}`);
+    });
 });
--- node.js/projects/raspbery-led-webserver/PIR/routes/index.js
+++ node.js/projects/raspbery-led-webserver/PIR/routes/index.js
@@ -1,31 +1,48 @@
 module.exports = function(io) {
   var express = require('express');
   var router = express.Router();
-  var Gpio = require('onoff').Gpio;
+  // var Gpio = require('onoff').Gpio;
 
-  const led = new Gpio(5, 'out');
+  // const led = new Gpio(5, 'out');
 
+  var armed = false;
 
   io.on('connection', (socket) => {
-    socket.emit('connected', {payload: 'Hello from server. You are connected'});
+    socket.emit('connected', {
+      payload: 'Hello from server. You are connected'
+    });
 
+    socket.on('getSystemStatus', (callback) => {
+      callback(armed);
+    });
+
+    socket.on('btnPressed', (btnPressed) => {
+      console.log('Button pressed - ' + btnPressed);
+      if (btnPressed === 'btn-on') {
+        armed = true;
+      } else {
+        armed = false;
+      }
+
+      socket.emit('newSystemStatus', armed);
+    });
   });
 
   /* GET home page. */
   router.get('/', function(req, res, next) {
-    var secSystemStatus = 'Disarmed';
+    var secSystemStatus = (armed ? "Armed": "Disarmed");
     res.render('index', { 
       title: 'Welcome to VR Security',
-      subtitle: `Security System is current ${secSystemStatus}`
+      subtitle: secSystemStatus
     });
   });
   
   process.on('SIGINT', () => {
-    led.unexport();
+    //led.unexport();
   });
 
   return router;
-}
+};
 
 
 
