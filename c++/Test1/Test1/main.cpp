224d14cc2dbf62278f4452e6dbc27b02c7a316f5
Radoslav Radoev <rradoev@gmail.com>
2019-09-18 00:16:09 -0700

    some c++

--- c++/Test1/Test1/main.cpp
+++ c++/Test1/Test1/main.cpp
@@ -7,8 +7,19 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
 int main() {
-    std::cout <<  "Hello Testing";
-    return 0;
+    vector<int> vector1;
+    vector<int> vector2;
+    
+    vector1.push_back(10);
+    vector1.push_back(20);
+    
+    cout << " --- Vector1 ---" << endl;
+    cout << "At index 0: " << vector1.at(0) << endl;
+    cout << "At index 1: " << vector1.at(1) << endl;
+    cout << "Vector1 size: " << vector1.size() << endl;
+    
 }
