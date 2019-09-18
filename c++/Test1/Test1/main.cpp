2d04594dfc8ac3d623173fc8d2cc9c7fe54423ad
Radoslav Radoev <rradoev@gmail.com>
2019-09-18 00:34:11 -0700

    some c++ vectors

--- c++/Test1/Test1/main.cpp
+++ c++/Test1/Test1/main.cpp
@@ -22,4 +22,31 @@ int main() {
     cout << "At index 1: " << vector1.at(1) << endl;
     cout << "Vector1 size: " << vector1.size() << endl;
     
+    
+    vector2.push_back(100);
+    vector2.push_back(200);
+    
+    cout << " --- Vector2 ---" << endl;
+    cout << "At index 0: " << vector2.at(0) << endl;
+    cout << "At index 1: " << vector2.at(1) << endl;
+    cout << "Vector2 size: " << vector2.size() << endl;
+    
+    vector< vector<int> > vector_2d;
+    vector_2d.push_back(vector1);
+    vector_2d.push_back(vector2);
+    
+    cout << " --- Vector_2d ---" << endl;
+    cout << "At index 0: " << vector_2d.at(0).at(0) << endl;
+    cout << "At index 1: " << vector_2d.at(1).at(0) << endl;
+    cout << "Vector_2d size: " << vector_2d.size() << endl;
+    
+    vector1.at(0) = 1000;
+    cout << "Updated value in vector1" << endl;
+    
+    cout << "At index 0: " << vector_2d.at(0).at(0) << endl;
+    cout << "At index 1: " << vector_2d.at(1).at(0) << endl;
+    
+    cout << "\n --- Vector1 ---" << endl;
+    cout << vector1.at(0) << endl;
+    cout << vector1.at(1) << endl;
 }
