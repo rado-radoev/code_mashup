c98a5171e14ef664ce2e01c5585f52d1fd9c3dc3
Radoslav Radoev <rradoev@gmail.com>
2019-10-15 21:39:12 -0700

    baby steps in react

--- react/monsters-app/monsters-rolodex/src/App.js
+++ react/monsters-app/monsters-rolodex/src/App.js
@@ -7,28 +7,24 @@ class App extends Component {
     super();
 
     this.state = {
-      string: "This is a test string"
+      monsters: []
     }
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState( {monsters: users} ));
+  }
+ 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.string}
-          </p>
-          <button onClick={() => this.setState({ string: 'Hello Rado' })} >Change Text</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          this.state.monsters.map(monster => (
+            <h1 key= { monster.id }>  { monster.name } </h1>
+          ))
+        }
       </div>
     );
   }
