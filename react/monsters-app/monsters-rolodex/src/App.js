9c6d0067909350fd064337d39203704342111427
Radoslav Radoev <rradoev@gmail.com>
2019-10-17 00:26:58 -0700

    baby steps in react

--- react/monsters-app/monsters-rolodex/src/App.js
+++ react/monsters-app/monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component} from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 
 class App extends Component {
   constructor() {
@@ -27,12 +28,10 @@ class App extends Component {
 
     return (
       <div className="App">
-      <input 
-        type="search" 
-        placeholder="search monsters" 
-        onChange={event => {
-          this.setState({searchField: event.target.value});
-      }}/>
+      <SearchBox
+        placeholder = "search monsters"
+        handleChangeFunction = {e => this.setState({searchField: e.target.value})}
+      />
       <CardList monsters={filteredMonsters} />        
       </div>
     );
