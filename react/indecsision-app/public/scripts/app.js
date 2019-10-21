44f5bdcf99a3c3a5a1855220b95127bbcd08b026
superklamer <rradoev@gmail.com>
2019-10-21 01:57:59 -0700

    indecsision app

--- react/indecsision-app/public/scripts/app.js
+++ react/indecsision-app/public/scripts/app.js
@@ -3,10 +3,54 @@
 console.log("app.js is running");
 
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecsision App"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Indecsision App"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is some paragrapsh"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item one"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item two"
+        )
+    )
 );
+
+var template1 = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Radoslav Radoev"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: 33"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: San Diego"
+    )
+);
+
 var appRoute = document.getElementById('app');
 
-ReactDOM.render(template, appRoute);
+ReactDOM.render(template1, appRoute);
--- react/indecsision-app/src/app.js
+++ react/indecsision-app/src/app.js
@@ -1,6 +1,25 @@
 console.log("app.js is running");
 
-var template = <h1>Indecsision App</h1>
+var template = (
+    <div>
+    <h1>Indecsision App</h1>
+    <p>This is some paragrapsh</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+    </div>
+);
+
+var template1 = (
+    <div>
+        <h1>Radoslav Radoev</h1>
+        <p>Age: 33</p>
+        <p>Location: San Diego</p>
+
+    </div>
+);
+
 var appRoute = document.getElementById('app');
 
-ReactDOM.render(template, appRoute);
+ReactDOM.render(template1, appRoute);
