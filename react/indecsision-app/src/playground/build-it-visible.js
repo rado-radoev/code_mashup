2f58d7c82199332f1cc80e4c0cc8417157f7d6b0
Radoslav Radoev <rradoev@gmail.com>
2019-10-27 23:22:20 -0700

    reacting to react

--- react/indecsision-app/src/playground/build-it-visible.js
+++ react/indecsision-app/src/playground/build-it-visible.js
@@ -1,34 +1,74 @@
-const appRoute = document.getElementById('app');
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
 
-const button = {
-    state: true
-}
+        this.state = {
+            visibility: false
+        };
 
-const toggleButton = () => {
-    button.state = !button.state;
-    // console.log(button.state);
-    render();
-}
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    }
 
-const render = () => {
- 
-    const template = ( 
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button id="btn" key='1' onClick={toggleButton}>
-                {button.state ? "Show Details" : "Hide Details"}
-            </button>
-            {!button.state && 
+    handleToggleVisibility() {
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button id="btn" key='1' onClick={this.handleToggleVisibility}>
+                 {this.state.visibility ? "Hide Details" : "Show Details"}
+                </button>
+                {this.state.visibility && 
                 <div>
                     <p>Hey. These are some details you can now see.</p>
                 </div>
-            }
-        </div>
+             }
+         </div>
+        );
+    };
+}
+
+
+ReactDOM.render(<VisibilityToggle/>, document.getElementById('app'));
+
+
+// const appRoute = document.getElementById('app');
+
+// const button = {
+//     state: true
+// }
+
+// const toggleButton = () => {
+//     button.state = !button.state;
+//     // console.log(button.state);
+//     render();
+// }
+
+// const render = () => {
+ 
+//     const template = ( 
+//         <div>
+//             <h1>Visibility Toggle</h1>
+//             <button id="btn" key='1' onClick={toggleButton}>
+//                 {button.state ? "Show Details" : "Hide Details"}
+//             </button>
+//             {!button.state && 
+//                 <div>
+//                     <p>Hey. These are some details you can now see.</p>
+//                 </div>
+//             }
+//         </div>
         
-    );
+//     );
 
-    ReactDOM.render(template, appRoute);
-};
+//     ReactDOM.render(template, appRoute);
+// };
 
-render();
+// render();
 
