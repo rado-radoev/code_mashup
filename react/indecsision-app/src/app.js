d4fb4396eff526189ad2b18562d5ddbcccaf1339
Radoslav Radoev <rradoev@gmail.com>
2019-10-31 00:31:15 -0700

    remove all does not work

--- react/indecsision-app/src/app.js
+++ react/indecsision-app/src/app.js
@@ -13,11 +13,24 @@ class IndecisionApp extends React.Component {
         this.handleDeleteOption = this.handleDeleteOption.bind(this);
     }
     componentDidMount() {
-        console.log('fetching data!');
+        try {
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+    
+            if (options) {
+                this.setState(() => ({options}));
+            }    
+        } catch (error) {
+            // Do nothing at all   
+        }        
     }
     
     componentDidUpdate(prevProps, prevState) {
-        console.log('saving data')
+        if (prevState.options.length !== this.state.options.length) {
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem('options', json);
+        }
+        
     }
 
     componentWillUnmount() {
@@ -109,6 +122,7 @@ const Options = (props) => {
     return (
         <div>
             <button onClick={props.handleDeleteOptions}>Remove all</button>
+            {props.options.length === 0 && <p>Please add an option to get started</p>}
             {
                 props.options.map((option) => (
                     <Option 
@@ -158,6 +172,10 @@ class AddOption extends React.Component {
        const error = this.props.handleAddOption(option);
 
         this.setState(() => ({ error }));
+
+        if (!error) {
+            e.target.elements.option.value = "";
+        }
     }
 
     render() {
