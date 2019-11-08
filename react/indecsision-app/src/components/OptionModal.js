a5eb37a69637d81fbd8a999a995bf2dde38f638c
Radoslav Radoev <rradoev@gmail.com>
2019-11-08 00:27:00 -0800

    added Modal

--- react/indecsision-app/src/components/OptionModal.js
+++ react/indecsision-app/src/components/OptionModal.js
@@ -5,6 +5,7 @@ const OptionModal = (props) => (
     <Modal
         isOpen={!!props.selectedOption}
         contentLabel="Selected Option"
+        onRequestClose={props.handleSelectedOption}
     >
         <h3>Selected Option</h3>
         {props.selectedOption && <p>{props.selectedOption}</p>}
