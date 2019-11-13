cb551aca6d069364cfa3d218ac4ac3e2a6525c93
Radoslav Radoev <rradoev@gmail.com>
2019-11-13 00:16:09 -0800

    im tired

--- react/indecsision-app/src/components/Options.js
+++ react/indecsision-app/src/components/Options.js
@@ -4,8 +4,10 @@ import Option from './Option';
 
 const Options = (props) => (
       <div>
-        <button onClick={props.handleDeleteOptions}>Remove All</button>
-        {props.options.length === 0 && <p>Please add an option to get started!</p>}
+        <button 
+          className="button button--link"
+          onClick={props.handleDeleteOptions}>Remove All</button>
+          {props.options.length === 0 && <p>Please add an option to get started!</p>}
         {
           props.options.map((option) => (
             <Option
