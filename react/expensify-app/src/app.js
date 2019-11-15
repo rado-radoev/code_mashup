7ceb5b09550a90995a26f89c21bee282e9f82d54
Radoslav Radoev <Radoslav.Radoev@sharp.com>
2019-11-15 14:00:57 -0800

    Restructure the app. Break into routers and components

--- react/expensify-app/src/app.js
+++ react/expensify-app/src/app.js
@@ -1,63 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
-const ExpenseDashbordPage = () => (
-    <div>
-        This is from my dashboard component
-    </div>
-);
-
-const AddExpensePage = () => (
-    <div>
-        Add expense component
-    </div>
-);
-
-const EditExpensePage = () => (
-    <div>
-        Edit expense component
-    </div>
-);
-
-const HelpPage = () => (
-    <div>
-        Help page component
-    </div>
-);
-
-const NotFoundPage = () => (
-    <div>
-        404 - <Link to="/">Go Home</Link>
-    </div>
-);
-
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true} >Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Get Help</NavLink>
-    </header>
-);
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Header />  
-
-            <Switch>
-                <Route path="/" component={ExpenseDashbordPage} exact={true} />
-                <Route path="/create" component={AddExpensePage}/>
-                <Route path="/edit" component={EditExpensePage} />
-                <Route path="/help" component={HelpPage}/>
-                <Route component={NotFoundPage}/>
-            </Switch>
-        </div>
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ react/expensify-app/src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+    <div>
+        Edit expense component
+    </div>
+);
+
+export default EditExpensePage;
