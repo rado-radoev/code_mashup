a1b54787c26694860656bbccbd5037fee3e8f29f
Radoslav Radoev <Radoslav.Radoev@sharp.com>
2019-11-20 15:29:31 -0800

    spreading arrays

--- react/expensify-app/src/playground/redux-expensify.js
+++ react/expensify-app/src/playground/redux-expensify.js
@@ -1,7 +1,28 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
 // ADD_EXPENSE
+const addExpense = (
+    { 
+        description ="", 
+        note = "", 
+        amount = 0, 
+        createdAt = 0 } = {}
+    ) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount, 
+        createdAt
+    }
+});
 // REMOVE_EXPENSE
+const removeExpense = ({id}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+});
 // EDIT_EXPENSE
 // SET_TEXT_FILTER
 // SORT_BY_DATE
@@ -13,6 +34,13 @@ import { createStore, combineReducers } from 'redux';
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch(action.type) {
+        case 'ADD_EXPENSE': 
+           return [
+               ...state,
+               action.expense
+           ];
+        case 'REMOVE_EXPENSE':
+            return state.filter(({ id }) => action.id !== id);
         default:
             return state;
     }
@@ -25,13 +53,13 @@ const filtersReducerDefaultState = {
     startDate: undefined,
     endDate: undefined
 };
+
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
         default:
             return state;
     }
 }
-
 
 // Store creation
 const store = createStore(
@@ -41,7 +69,14 @@ const store = createStore(
     })
 );
 
-console.log(store.getState());
+store.subscribe( () => {
+    console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 const demo = {
     expenses: [{
