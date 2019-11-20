421a4395d4318adf3baf657fde72bea90d234e68
Radoslav Radoev <rradoev@gmail.com>
2019-11-20 00:30:00 -0800

    more destructuring

--- react/expensify-app/src/playground/destructuring.js
+++ react/expensify-app/src/playground/destructuring.js
@@ -1,3 +1,8 @@
+//
+// Object destructuring
+//
+
+
 // const person = {
 //     name: 'Rado',
 //     age: 35,
@@ -22,13 +27,30 @@
 // };
 
 
-const book = {
-    titile: 'Ego is the enemy',
-    author:  'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-};
+// const book = {
+//     titile: 'Ego is the enemy',
+//     author:  'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
 
-const { name: publisherName = 'Self-Published' } = book.publisher
-console.log(publisherName);
+// const { name: publisherName = 'Self-Published' } = book.publisher
+// console.log(publisherName);
+
+
+
+
+
+
+//
+// Array destructuring
+//
+
+const address = ['1299 S Juniper Street', 'Philadelphia', 'Pensylvania', '19174'];
+const [ , , state = 'New York'] = address;
+console.log(`You are in  ${state}`)
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+const [ size, , price ] = item;
+console.log(`A medium ${size} costs ${price}`)
