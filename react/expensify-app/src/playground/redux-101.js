9bfd8823332abe6031778e8bd2251e7ff40aea9b
Radoslav Radoev <Radoslav.Radoev@sharp.com>
2019-11-19 16:30:38 -0800

    stuff

--- react/expensify-app/src/playground/redux-101.js
+++ react/expensify-app/src/playground/redux-101.js
@@ -1,8 +1,25 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 } ) => {
-    return state;
+const store = createStore((state = { count: 0 }, action ) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        default:
+            return state;
+    }
 });
+
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
 
 console.log(store.getState());
 
