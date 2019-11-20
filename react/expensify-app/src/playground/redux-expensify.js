cc331907ad31de519d4e8609c411f66179159537
Radoslav Radoev <Radoslav.Radoev@sharp.com>
2019-11-20 14:19:28 -0800

    reducers

--- react/expensify-app/src/playground/redux-expensify.js
+++ react/expensify-app/src/playground/redux-expensify.js
@@ -1,5 +1,48 @@
 import { createStore, combineReducers } from 'redux';
 
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+// Expenses reducer
+const expensesReducerDefaultState = [];
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
+
+// Filters reducer
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+
+// Store creation
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);
+
+console.log(store.getState());
+
 const demo = {
     expenses: [{
         id: 'idgoeshere',
