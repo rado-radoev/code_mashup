cae9f7b1db7ac9b15d633496b381eecb21702fc8
Radoslav Radoev <rradoev@gmail.com>
2019-11-20 00:07:18 -0800

    es6 destructuring

--- react/expensify-app/src/playground/destructuring.js
+++ react/expensify-app/src/playground/destructuring.js
@@ -1,22 +1,34 @@
-const person = {
-    name: 'Rado',
-    age: 35,
-    location: {
-        city: 'San Diego',
-        temp: 92
+// const person = {
+//     name: 'Rado',
+//     age: 35,
+//     location: {
+//         city: 'San Diego',
+//         temp: 92
+//     }
+// };
+
+
+// const { name: firstName = 'Anonymous', age } = person;
+// // const name = person.name;
+// // const age = person.age;
+
+
+// console.log(`${firstName} is ${age}.`)
+
+
+// const { city, temp: temperature } = person.location;
+// if (city && temperature) {
+//     console.log(`It is ${temperature} in ${city}`)
+// };
+
+
+const book = {
+    titile: 'Ego is the enemy',
+    author:  'Ryan Holiday',
+    publisher: {
+        name: 'Penguin'
     }
 };
 
-
-const { name = 'Anonymous', age } = person;
-// const name = person.name;
-// const age = person.age;
-
-
-console.log(`${name} is ${age}.`)
-
-
-const { city, temp: temperature } = person.location;
-if (city && temperature) {
-    console.log(`It is ${temperature} in ${city}`)
-};
+const { name: publisherName = 'Self-Published' } = book.publisher
+console.log(publisherName);
