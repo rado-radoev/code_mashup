c007b5904cca62ea41117b7ddd322d59ec419fb9
Radoslav Radoev <Radoslav.Radoev@sharp.com>
2019-11-21 14:52:21 -0800

    higher order component

---
+++ react/expensify-app/src/playground/hoc.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Info = (props) => (
+    <div>
+        <h1>Info</h1>
+        <p>The info is: {props.info}</p>
+    </div>
+);
+
+const withAdminWarning = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            { props.isAdmin && <p>This is private info. Please don't share.</p> }
+            <WrappedComponent {...props}/>
+        </div>
+    );
+};
+
+const AdminInfo = withAdminWarning(Info);
+
+ReactDOM.render(<AdminInfo isAdmin={true} info="This are the details" />, document.getElementById('app'));
