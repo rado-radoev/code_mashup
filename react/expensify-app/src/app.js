bb98299a9c90ea92d4a4e9cab9b56ea311f07d37
Radoslav Radoev <Radoslav.Radoev@sharp.com>
2019-11-21 14:21:28 -0800

    refactoring

--- react/expensify-app/src/app.js
+++ react/expensify-app/src/app.js
@@ -2,14 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import getVisibleExpenses from './selectors/expenses';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
 const store = configureStore();
 
+store.subscribe( () => {
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
+});
+
 // addExpense -> Water bill
+store.dispatch(addExpense({description: 'Water bill'}));
 // addExpense -> Gas bill
+store.dispatch(addExpense({description: 'Gas bill'}));
 // setTextFilter -> bill (2 items) -> water (1 item)
+store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
 // getVisibleExpenses -> print visible ones to screen
+
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
