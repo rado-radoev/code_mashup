2bfa2edfe2475c2e327f6c20788abefb1f73cc89
Radoslav Radoev <rradoev@gmail.com>
2020-01-16 01:16:44 -0800

    node

--- node.js/projects/wsrvr/src/app.js
+++ node.js/projects/wsrvr/src/app.js
@@ -1,12 +1,20 @@
 const path = require('path')
 const express = require('express')
-
+const hbs = require('hbs')
 
 const app = express();
 
+// Define paths for express config
 const publicDirPath = path.join(__dirname, '../public/')
+const viewsDirPath = path.join(__dirname, '../templates/views')
+const partialsDirPath = path.join(__dirname, '../templates/partials/')
 
-app.set('view engine', 'hbs')
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsDirPath);
+hbs.registerPartials(partialsDirPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirPath))
 
 app.get('', (req, res) => {
