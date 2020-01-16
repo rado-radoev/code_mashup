2a00aff86dc8b9b66d6428a4e97d96ff132c3e9b
Radoslav Radoev <rradoev@gmail.com>
2020-01-16 00:36:27 -0800

    node

---
+++ node.js/projects/wsrvr/src/app.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const express = require('express')
+
+
+const app = express();
+
+const publicDirPath = path.join(__dirname, '../public/')
+app.use(express.static(publicDirPath))
+
+
+
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Pleven'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
