0d582fbd53af9614e4fb366224966c0c67a634cf
Radoslav Radoev <rradoev@gmail.com>
2020-01-16 01:01:04 -0800

    node

--- node.js/projects/wsrvr/src/app.js
+++ node.js/projects/wsrvr/src/app.js
@@ -5,10 +5,30 @@ const express = require('express')
 const app = express();
 
 const publicDirPath = path.join(__dirname, '../public/')
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirPath))
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Rado'
+    })
+})
 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About page',
+        name: 'Rado'
+    })
+})
 
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help page title',
+        name: 'Rado'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
