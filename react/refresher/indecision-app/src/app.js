c7b9e1b03eb4e1f6a503924b2ab823a3526a6607
Radoslav Radoev <rradoev@gmail.com>
2020-01-17 22:31:18 -0800

    more react

--- react/refresher/indecision-app/src/app.js
+++ react/refresher/indecision-app/src/app.js
@@ -1,9 +1,16 @@
 console.log('app.js is running')
 
+const app = {
+    title: 'Indecision app',
+    subtitle: 'Let the computer decide for you',
+    options: ['One', 'Two']
+}
+
 var template = (
  <div>
-    <h1>Indecision App</h1>
-    <p>Some info to not make decision on</p>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
     <ol>
         <li>Item one</li>
         <li>Item two</li>
@@ -11,9 +18,25 @@ var template = (
  </div>
 );
 
+var user = {
+    name: 'Rado',
+    age: 36,
+    location: 'Sacramento'
+}
 
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>
+    } 
+}
 
-
+var templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+);
 
 
 const appRoot = document.getElementById('app')
