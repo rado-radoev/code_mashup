61fbd040f1e9877f3955e28a3ebe1822665de27d
Radoslav Radoev <rradoev@gmail.com>
2020-01-26 23:21:50 -0800

    setting up babel and webpack

--- react/refresher/indecision-app/src/app.js
+++ react/refresher/indecision-app/src/app.js
@@ -6,9 +6,14 @@
 // console.log(add(4,5));
 // console.log(subtract(100, 91));
 
-import isSenior, {isAdult, canDrink} from './person.js'
+// import isSenior, {isAdult, canDrink} from './person.js'
 
-console.log(isAdult(25))
-console.log(canDrink(16))
-console.log(isSenior(55));
+// console.log(isAdult(25))
+// console.log(canDrink(16))
+// console.log(isSenior(55));
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = <p>testing 123</p>
+ReactDOM.render(template, document.getElementById('app'));
