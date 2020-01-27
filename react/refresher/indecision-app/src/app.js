6ba60aca0d372d09adaf2f1925a67ab4ba0da16f
Radoslav Radoev <rradoev@gmail.com>
2020-01-26 23:38:11 -0800

    moved all components to their own files

--- react/refresher/indecision-app/src/app.js
+++ react/refresher/indecision-app/src/app.js
@@ -1,19 +1,6 @@
 // import subtract, { square, add } from './utils.js'
-
-
-// console.log('app.js is running!');
-// console.log(square(4))
-// console.log(add(4,5));
-// console.log(subtract(100, 91));
-
-// import isSenior, {isAdult, canDrink} from './person.js'
-
-// console.log(isAdult(25))
-// console.log(canDrink(16))
-// console.log(isSenior(55));
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp'
 
-const template = <p>testing 123</p>
-ReactDOM.render(template, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp options={['Option One', 'Option Two']}/>, document.getElementById('app'));
