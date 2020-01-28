fa4b682f6b83240ce8d8329f70ffdd100941a4d9
Radoslav Radoev <rradoev@gmail.com>
2020-01-27 23:33:15 -0800

    styling

--- react/refresher/indecision-app/src/app.js
+++ react/refresher/indecision-app/src/app.js
@@ -1,7 +1,8 @@
 // import subtract, { square, add } from './utils.js'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp'
+import IndecisionApp from './components/IndecisionApp';
+import './styles/styles.scss';
 
 
 ReactDOM.render(<IndecisionApp options={['Option One', 'Option Two']}/>, document.getElementById('app'));
