3cb4719e9f93cd2721a27c5f31510d456c56165c
Radoslav Radoev <rradoev@gmail.com>
2020-01-30 00:06:50 -0800

    epxensify app boilerplate

---
+++ react/refresher/boilerplate-v1/src/app.js
@@ -0,0 +1,9 @@
+// import subtract, { square, add } from './utils.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css'
+import './styles/styles.scss';
+
+
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+
