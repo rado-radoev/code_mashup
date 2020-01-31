49279de01db3e02f50b9cf0d73c5907887c8fc05
Radoslav Radoev <rradoev@gmail.com>
2020-01-30 23:27:21 -0800

    rect router query params

--- react/refresher/expensify-app/src/components/EditxpensePage.js
+++ react/refresher/expensify-app/src/components/EditxpensePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const EditxpensePage = () => (
-    <div>
-        This is my edit expense component
-    </div>
-);
+const EditxpensePage = (props) => {
+    console.log(props)
+    return (
+        <div>
+            Editing expense with id {props.match.params.id}
+        </div>
+    );
+};
 
 export default EditxpensePage;
