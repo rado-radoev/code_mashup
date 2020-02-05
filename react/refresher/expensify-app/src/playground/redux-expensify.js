1f50582ad753f963f0ff73b3d2aaabaf42e8c622
Radoslav Radoev <rradoev@gmail.com>
2020-02-05 00:06:10 -0800

    more reducers

--- react/refresher/expensify-app/src/playground/redux-expensify.js
+++ react/refresher/expensify-app/src/playground/redux-expensify.js
@@ -1,9 +1,41 @@
 import {createStore, combineReducers} from 'redux';
+import uuid from 'uuid';
 
 // ADD_EXPENSE
+const addExpense = (
+    { 
+        description = '', 
+        note = '', 
+        amount = 0, 
+        createdAt = 0 
+    } = {}
+   ) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
 // REMOVE_EXPENSE
+const removeExpense = ({id} = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+
+});
 // EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
 // SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
 // SET_START_DATE
@@ -15,6 +47,24 @@ const expensesReducerDefaultState = []
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [
+                ...state,
+                action.expense
+            ]
+        case 'REMOVE_EXPENSE':
+            return state.filter(({id}) => id !== action.id);
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    };
+                } else {
+                    return expense;
+                }
+            });
         default:
             return state;
     };
@@ -31,6 +81,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            }
         default:
             return state;
     }
@@ -44,7 +99,22 @@ const store = createStore(
     })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+const expense1 = store.dispatch(addExpense({description:'Rent', amount:100}));
+const expense2 = store.dispatch(addExpense({description:'Coffee', amount:300}));
+
+console.log(expense1.expense.id)
+console.log(expense2.expense.id)
+
+const rem1 = store.dispatch(removeExpense({id: expense1.expense.id}));
+
+store.dispatch(editExpense(expense2.expense.id, { amount: 500 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
 
 const demoState = {
     expenses: [{
