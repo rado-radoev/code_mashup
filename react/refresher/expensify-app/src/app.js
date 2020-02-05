20fc5abe9a836b224dac9264c41150064b345445
Radoslav Radoev <rradoev@gmail.com>
2020-02-05 15:08:16 -0800

    cant do this at work

--- react/refresher/expensify-app/src/app.js
+++ react/refresher/expensify-app/src/app.js
@@ -2,9 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './router/AppRouter';
+import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
+const store = configureStore();
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
 
