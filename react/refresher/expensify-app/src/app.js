cdfb2206f4a4eeee229b19f26beef735f15d1b90
Radoslav Radoev <rradoev@gmail.com>
2020-02-05 23:09:04 -0800

    a

--- react/refresher/expensify-app/src/app.js
+++ react/refresher/expensify-app/src/app.js
@@ -5,12 +5,20 @@ import AppRouter from './router/AppRouter';
 import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
 
+store.dispatch(addExpense({description: 'Water bill', amount: 300}));
+store.dispatch(addExpense({description: 'Gas bill', amount: 150}));
+store.dispatch(setTextFilter('water'));
 
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
-console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
 
