dbb3c51e1102fc1e6124247b7abf8e799fa289ec
Radoslav Radoev <rradoev@gmail.com>
2020-02-06 23:10:40 -0800

    react-redux connect

--- react/refresher/expensify-app/src/app.js
+++ react/refresher/expensify-app/src/app.js
@@ -1,6 +1,7 @@
 // import subtract, { square, add } from './utils.js'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider, connect} from 'react-redux';
 import AppRouter from './router/AppRouter';
 import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css'
@@ -15,10 +16,19 @@ store.dispatch(addExpense({description: 'Water bill', amount: 300}));
 store.dispatch(addExpense({description: 'Gas bill', amount: 150}));
 store.dispatch(setTextFilter('water'));
 
+setTimeout(() => {
+    store.dispatch(setTextFilter('rent'));
+}, 3000)
+
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
+const jsx = (
+   <Provider store={store}>
+        <AppRouter />
+   </Provider>
+);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
 
