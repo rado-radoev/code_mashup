1a0c69de1735e89cf0a2605751f04cdb617996bf
Radoslav Radoev <rradoev@gmail.com>
2020-02-18 00:01:45 -0800

    pssst

--- react/refresher/expensify-app/src/app.js
+++ react/refresher/expensify-app/src/app.js
@@ -7,9 +7,11 @@ import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 import {addExpense, startSetExpenses} from './actions/expenses';
+import {login, logout} from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import {firebase} from './firebase/firebase';
 import {history} from './router/AppRouter';
+
 
 const store = configureStore();
 
@@ -46,6 +48,7 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
    if (user) {
+      store.dispatch(login(user.uid));
       store.dispatch(startSetExpenses()).then(() => {
          renderApp();
          if (history.location .pathname === '/') {
@@ -53,6 +56,7 @@ firebase.auth().onAuthStateChanged((user) => {
          }
       });
    } else {
+      store.dispatch(logout());
       renderApp();
       history.push('/');
    }
