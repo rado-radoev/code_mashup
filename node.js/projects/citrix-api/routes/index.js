9c66b6ce1d1778c21e40f0a500c56945ad426dfe
Radoslav Radoev <rradoev@gmail.com>
2020-02-28 13:39:47 -0800

    added a few more routes to index.js

--- node.js/projects/citrix-api/routes/index.js
+++ node.js/projects/citrix-api/routes/index.js
@@ -5,8 +5,117 @@ var db = require('../utils/db/db');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let result = db.Query();
-  res.send(result.length)
-  //res.render('index', { title: 'Express' });
+  result.then((resul) => {
+    var jsonStr = JSON.stringify(resul.recordset);
+    res.json(JSON.parse(jsonStr));
+  }).catch((e) => {
+    console.log(e)
+  })
+  // res.render('index', { title: 'Express' });
 });
+
+router.get('/reboots/location/:loc/:name', (req,res,next) => {
+  db.rebootSchedules(req.params.loc)
+    .then((r) => {
+      const deliveryGroups = r.recordset;
+      const filteredDeliveryGroups = deliveryGroups.filter(deliveryGroup => deliveryGroup.Location.toLowerCase() === req.params.loc.toLowerCase())
+      
+      if (req.params.name) {
+        const specificDeliveryGroup = filteredDeliveryGroups.filter(dgs => dgs.DesktopGroupName.toLowerCase() === req.params.name.toLowerCase() )
+        const filteredDeliveryGroupsString = JSON.stringify(specificDeliveryGroup);
+        const filteredDeliveryGroupsJSON = JSON.parse(filteredDeliveryGroupsString);
+        res.json(filteredDeliveryGroupsJSON);
+      }
+
+
+  }).catch((e) => {
+    res.send(e).status(400);
+  })
+});
+
+router.get('/reboots/location/:loc', (req, res) => {
+  db.rebootSchedules(req.params.loc)
+    .then((r) => {
+      const deliveryGroups = r.recordset;
+      const filteredDeliveryGroups = deliveryGroups.filter(deliveryGroup => deliveryGroup.Location.toLowerCase() === req.params.loc.toLowerCase())
+      const filteredDeliveryGroupsString = JSON.stringify(filteredDeliveryGroups);
+      const filteredDeliveryGroupsJSON = JSON.parse(filteredDeliveryGroupsString);
+      res.json(filteredDeliveryGroupsJSON);
+  }).catch((e) => {
+    res.send(e).status(400);
+  })
+});
+
+router.get('/servers', (req, res) => {
+  db.servers().then((s) => {
+    let servers = s.recordset;
+    const serversString = JSON.stringify(servers);
+    const serversJson = JSON.parse(serversString);
+    let newArr = serversJson.map((jf) => {
+      jf.PublishedApplications = JSON.parse(jf.PublishedApplications)
+      return jf;
+    });
+    res.json(newArr);
+  }).catch((e) => {
+    res.send(e).status(400)
+  })  
+}); 
+
+router.get('/applications/:uid', (req, res) => {
+  db.applications().then((a) => {
+    let apps = a.recordset;
+    let filteredApps = apps.filter((app) => app.Uid === parseInt(req.params.uid));
+    let filteredAppsString = JSON.stringify(filteredApps);
+    let filteredAppsJson = JSON.parse(filteredAppsString);
+    let newArr = filteredAppsJson.map((jf) => {
+      jf.AssociatedDesktopGroupUids = JSON.parse(jf.AssociatedDesktopGroupUids)
+      jf.AssociatedUserFullNames = JSON.parse(jf.AssociatedUserFullNames)
+      jf.AssociatedUserNames = JSON.parse(jf.AssociatedUserNames)
+      jf.AssociatedUserUPNs = JSON.parse(jf.AssociatedUserUPNs)
+      jf.CommandLineArguments = JSON.parse(jf.CommandLineArguments)
+      return jf;
+    });
+    res.json(newArr);
+  }).catch((e) => {
+    res.send(e).status(400);
+  })
+});
+
+
+router.get('/applications', (req, res) => {
+  db.applications().then((a) => {
+    let apps = a.recordset;
+    let appsString = JSON.stringify(apps);
+    let appsJson = JSON.parse(appsString);
+    let newArr = appsJson.map((jf) => {
+      jf.AssociatedDesktopGroupUids = JSON.parse(jf.AssociatedDesktopGroupUids)
+      jf.AssociatedUserFullNames = JSON.parse(jf.AssociatedUserFullNames)
+      jf.AssociatedUserNames = JSON.parse(jf.AssociatedUserNames)
+      jf.AssociatedUserUPNs = JSON.parse(jf.AssociatedUserUPNs)
+      jf.CommandLineArguments = JSON.parse(jf.CommandLineArguments)
+      return jf;
+    });
+    res.json(newArr);
+  }).catch((e) => {
+    res.send(e).status(400);
+  })
+});
+
+router.get('/deliveryGroups', (req, res) => {
+  db.deliveryGroups()
+    .then((d) => {
+      let dgs = d.recordset;
+      let dgString = JSON.stringify(dgs);
+      let dgJson = JSON.parse(dgString);
+      let newArr = dgJson.map((dg) => {
+        dg.Tags = JSON.parse(dg.Tags);
+        return dg;
+      });
+      res.json(newArr);
+    }).catch((e) => {
+      res.send(e).status(400);
+    })
+});
+
 
 module.exports = router;
--- node.js/projects/citrix-api/utils/db/db.js
+++ node.js/projects/citrix-api/utils/db/db.js
@@ -2,8 +2,8 @@ const sql = require('mssql');
 require('msnodesqlv8');
 
 const config = {
-    user: 'radra',
-    password: 'Superzebra85',
+    user: process.env.USER,
+    password: process.env.PASSWORD,
     database: process.env.DATABASE,
     server: process.env.DB_HOST,
     domain: process.env.DOMAIN,
@@ -14,18 +14,57 @@ const config = {
     parseJSON: true
 }
 
-exports.Query = async () => {
+const rebootSchedules = async (location) => {
     try {
         let pool = new sql.ConnectionPool(config); 
-        let conn = await pool.connect()
-        let result1 = await pool.request().query('SELECT * FROM dbo.RSM').then(result => {
-            return result;
-        })
+        let conn = await pool.connect();
+        let result1 = await pool.request().query(`SELECT * FROM dbo.${location}`)
         return result1
     } catch (error) {
         console.log(error)
     }
 };
+
+const servers = async () => {
+    try {
+        let pool = new sql.ConnectionPool(config);
+        let conn = await pool.connect();
+        let result = await pool.request().query(`SELECT * FROM dbo.Servers`)
+        return result;
+    } catch (error) {
+        return error;
+    }
+}
+
+const applications = async () => {
+    try {
+        let pool = new sql.ConnectionPool(config);
+        let conn = await pool.connect();
+        let result = await pool.request().query('SELECT * FROM dbo.Applications');
+        return result;
+    } catch (error) {
+        return error
+    }
+}
+
+const deliveryGroups = async() => {
+    try {
+        let pool = new sql.ConnectionPool(config);
+        let conn = await pool.connect();
+        let result = await pool.request().query('SELECT * FROM dbo.DeliveryGroups')
+        console.log(result)
+        return result;
+    } catch (error) {
+        return error;
+    }
+};
+
+module.exports = {
+    rebootSchedules,
+    servers,
+    applications,
+    deliveryGroups
+}
 
 // sql.on('error', err => {
 //     console.log(err)
